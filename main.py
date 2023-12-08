253cdcd5312d64b30d50acd0fd9b5a0b64c398be
Kharkov Timur <timur.kharkov@gmail.com>
2023-12-08 10:16:02 +0200

    Implemented contact bot on classes. Added instruction to README file

--- main.py
+++ main.py
@@ -1,1 +1,81 @@
-print('Hello world')
+from collections import UserDict
+import re
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    def __init__(self, value):
+        if not self.validate(value):
+            raise ValueError(f"Phone number {value} is invalid")
+        super().__init__(value)
+
+    def validate(self, phone):
+        return re.fullmatch(r"\d{10}", phone) is not None
+
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        self.phones.remove(Phone(phone))
+
+    def edit_phone(self, old_phone, new_phone):
+        for i, phone in enumerate(self.phones):
+            if phone.value == old_phone:
+                self.phones[i] = Phone(new_phone)
+                return
+        raise ValueError(f"Phone number {old_phone} not found in contact")
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        return None
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(str(p) for p in self.phones)}"
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+
+
+# Test code example
+# book = AddressBook()
+# john_record = Record("John")
+# john_record.add_phone("1234567890")
+# john_record.add_phone("5555555555")
+# book.add_record(john_record)
+# jane_record = Record("Jane")
+# jane_record.add_phone("9876543210")
+# book.add_record(jane_record)
+# for name, record in book.data.items():
+#     print(record)
+# john_record = book.find("John")
+# print(john_record)
+# john_record.edit_phone("1234567890", "1112223333")
+# print(john_record)
